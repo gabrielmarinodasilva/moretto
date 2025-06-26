442b83e73524f63285c139c1d46201c470dad953
Jorge Gabriel <105565673+jorgegabrielsl@users.noreply.github.com>
2025-06-26 01:00:51 -0300

    ajuste final

--- moretto/src/main.jsx
+++ moretto/src/main.jsx
@@ -1,19 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './index.css'
 
-import Home from './pages/Home'
-import Main from './pages/main'
-import Master from './pages/Master'
+
+import Autor from './pages/autor/autor'
+import Livro from './pages/livro/livro'
+import Home from './pages/home/home'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/Autor" element={<Home />} />
-        <Route path="/livros" element={<Main />} />
-        <Route path='/Home' element={<Master />}/>
+        <Route path="/Autor" element={<Autor />} />
+        <Route path="/livros" element={<Livro />} />
+        <Route path='/Home' element={<Home />}/>
       </Routes>
     </BrowserRouter>
   </StrictMode>
